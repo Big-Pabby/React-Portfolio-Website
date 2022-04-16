f935366dfe3289c9f23d3edd28934e53f87a24d9
Victor <victoradekunle312@gmail.com>
2022-04-16 20:26:09 +0000

    creates the Header Component

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './nav.css'
+import {BiHomeAlt} from 'react-icons/bi'
+
+const Nav = () => {
+  return (
+    <nav>
+      <a href="#"><BiHomeAlt /></a>
+    </nav>
+  )
+}
+
+export default Nav
