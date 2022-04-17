34634c5c8702cb352460d6a68d46e2172f622664
Victor <victoradekunle312@gmail.com>
2022-04-17 20:37:02 +0000

    Creates the About Components

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import './about.css'
+import AboutMe from '../../assets/images/about2.jpg'
+import {FaAward} from 'react-icons/fa'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={AboutMe} alt="aboutPic" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>2+ Years Working Experience</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>10+ Completed Projects</small>
+            </article>
+          </div>
+
+          <p>
+            Hi, I'm Victor Adekunle a 400lvl Physics Electronics Student in Federal University of Technology, Akure. Some people know me as Pabby a Full Stack Web Developer that is heavy on the Front End part of the Web. I make use of tools like: HTML/CSS, Javascript, PostgresSQL and other included <a href="#experience">frameworks</a>(click on the frameworks to view), Would love to create lots of exciting Projects for you.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
